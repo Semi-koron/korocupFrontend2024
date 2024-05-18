2382d8234dd0285739e4763294edafb33b6242f6
ryomahatakenaka <hatakenaka.ryoma425@mail.kyutech.jp>
2024-05-18 14:49:35 +0900

    commentボタンできました

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,9 @@
 import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import { getAuth } from "firebase/auth";
 import { app } from "./firebase/firebase";
+import buttons from "./page.module.css";
+import Likebutton from "./components/buttons/LIkebutton/page";
+import Commentbutton from "./components/buttons/comentbutton/page";
 
 const auth = getAuth(app);
 
@@ -14,6 +17,8 @@ export default function Home() {
   return (
     <>
       <h1>Hello World</h1>
+      <Likebutton />
+      <Commentbutton />
     </>
   );
 }
