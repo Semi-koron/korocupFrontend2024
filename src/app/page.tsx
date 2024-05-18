ca237a3af946c11731a47aa5b8cc50ef6bc0c2ac
ryomahatakenaka <hatakenaka.ryoma425@mail.kyutech.jp>
2024-05-18 15:07:44 +0900

    いらないタグを削除

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,6 @@
 import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import { getAuth } from "firebase/auth";
 import { app } from "./firebase/firebase";
-import buttons from "./page.module.css";
-import Likebutton from "./components/buttons/LIkebutton/page";
-import Commentbutton from "./components/buttons/comentbutton/page";
 
 const auth = getAuth(app);
 
@@ -17,8 +14,6 @@ export default function Home() {
   return (
     <>
       <h1>Hello World</h1>
-      <Likebutton />
-      <Commentbutton />
     </>
   );
 }
