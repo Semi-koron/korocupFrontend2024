de7c09a3ca98abf214841422ebd067ce8f94ceb3
Semi-koron <6jimoto6@gmail.com>
2024-05-18 18:21:55 +0900

    create paint function

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@
 import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import { getAuth } from "firebase/auth";
 import { app } from "./firebase/firebase";
+import Link from "next/link";
 
 const auth = getAuth(app);
 
@@ -14,6 +15,8 @@ export default function Home() {
   return (
     <>
       <h1>Hello World</h1>
+      <Link href="pages/painting">Painting</Link>
+      <button onClick={login}>login</button>
     </>
   );
 }
