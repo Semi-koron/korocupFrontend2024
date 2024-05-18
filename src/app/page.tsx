dcc34cea6188e8adcb14551765353433af878b34
ryomahatakenaka <hatakenaka.ryoma425@mail.kyutech.jp>
2024-05-18 15:20:31 +0900

    ダウンロードボタンできました

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,10 @@
 import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import { getAuth } from "firebase/auth";
 import { app } from "./firebase/firebase";
+import buttons from "./page.module.css";
+import Likebutton from "./components/buttons/LIkebutton/page";
+import Comentbutton from "./components/buttons/comentbutton/page";
+import Downloadbutton from "./components/buttons/downloadbutton/page";
 
 const auth = getAuth(app);
 
@@ -14,6 +18,9 @@ export default function Home() {
   return (
     <>
       <h1>Hello World</h1>
+      <Likebutton />
+      <Comentbutton />
+      <Downloadbutton />
     </>
   );
 }
